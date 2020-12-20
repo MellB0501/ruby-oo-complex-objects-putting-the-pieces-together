d7558cabe1fb316a0881c4e6640483ae323c467a
Mell Basham <mellbasham@gmail.com>
2020-12-19 19:41:16 -0700

    commit tree 918d602613ce4647bbbab03a4cf4a5a67d3e70c8

--- README.md
+++ README.md
@@ -14,7 +14,6 @@ problem on your own.
 1. Read through the tutorial, following along as you go.
 2. Once you've finished, make the tests in `spec/01_book_spec.rb` pass. Run only
    those tests by typing `learn spec/01_book_spec.rb` on your command line.
-
 ## Tutorial
 
 Object-oriented programming, or OOP, is an extremely useful programming paradigm
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+class Shoe 
+attr_accessor :brand, :color, :size, :material, :condition, :cobble
+
+    def initialize(brand, color, size, material, condition, cobble)
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+        @cobble = cobble
+    end
+
+
+end
